f9c237789a49031df786f476e6775af140c35bd0
Charles Yin <c_yin@wargaming.net>
2015-07-02 15:53:05 +1000

    Remove not used projects from maya plugin solution

--- build/cmake/BWMayaProject.cmake
+++ build/cmake/BWMayaProject.cmake
@@ -50,54 +50,18 @@ SET( BW_LIBRARY_PROJECTS
 
 	#NGT Systems
 	variant				core/lib/variant
-	dependency_system	core/lib/dependency_system
-	command_system		core/lib/command_system
-	control_system		core/lib/control_system
-	reflection			core/lib/reflection
-	reflection_utils	core/lib/reflection_utils
-	serialization		core/lib/serialization
-	string_utils		core/lib/string_utils
-	logging_system		core/lib/logging_system
 	ngt_core_common		core/lib/ngt_core_common
-	copy_paste			core/lib/copy_paste_system
+	dependency_system	core/lib/dependency_system
+	ui_framework        core/lib/ui_framework
 
 	#Tools Common
 	logging				core/lib/logging
 	generic_plugin		core/lib/generic_plugin
 	generic_plugin_manager	core/lib/generic_plugin_manager
-	qt_common			core/lib/qt_common
-	qt_script			core/lib/qt_script
-	data_model			core/lib/data_model
-	ui_framework		core/lib/ui_framework
-
 )
 
 SET( BW_BINARY_PROJECTS	
 	# Apps
-	generic_app			core/app/generic_app
 	maya_plugin			core/app/maya_plugin
-	
-)
-
-SET( BW_PLUGIN_PROJECTS
-	# Plugins
-	#plg_automation				core/plugins/plg_automation
-	plg_reflection				core/plugins/plg_reflection
-	plg_command_system			core/plugins/plg_command_system
-	plg_type_system				core/plugins/plg_type_system
-	plg_editor_interaction		core/plugins/plg_editor_interaction
-	plg_control_system			core/plugins/plg_control_system
-	plg_history_ui				core/plugins/plg_history_ui
-	plg_macros_ui				core/plugins/plg_macros_ui
-	plg_qt_app					core/plugins/plg_qt_app
-	plg_qt_common				core/plugins/plg_qt_common
-	plg_serialization			core/plugins/plg_serialization
-	plg_progress_manager		core/plugins/plg_progress_manager
-	plg_idedebug_logger			core/plugins/plg_idedebug_logger
-	plg_logging_system			core/plugins/plg_logging_system
-	plg_alert_ui				core/plugins/plg_alert_ui
-	plg_file_system				core/plugins/plg_file_system
-	plg_asset_browser			core/plugins/plg_asset_browser
-	plg_copy_paste				core/plugins/plg_copy_paste
 )
 

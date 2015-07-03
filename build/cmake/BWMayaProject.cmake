7f6d246035f53781f041dd23fa493db3a5a5575b
Charles Yin <c_yin@wargaming.net>
2015-07-03 17:54:32 +1000

    Remove maya_plugin specific cmake configuration from BWMayaProject.cmake
    Move them into BWConfiguration_maya_plugin.cmake

--- build/cmake/BWMayaProject.cmake
+++ build/cmake/BWMayaProject.cmake
@@ -38,29 +38,3 @@ INCLUDE_DIRECTORIES( ${BW_SOURCE_DIR}/bw/lib )
 
 INCLUDE_DIRECTORIES( ${MAYA_DEVKIT_DIR}/include )
 INCLUDE_DIRECTORIES( ${MAYA_QT_HEADER_DIR} )
-
-SET( TARGET_PLUGIN_APP_FOLDER_NAME generic_app_test )
-
-
-SET( BW_LIBRARY_PROJECTS	
-
-	#WG Systems
-	wg_types			core/lib/wg_types 
-	wg_memory			core/lib/wg_memory
-
-	#NGT Systems
-	variant				core/lib/variant
-	ngt_core_common		core/lib/ngt_core_common
-	dependency_system	core/lib/dependency_system
-	ui_framework        core/lib/ui_framework
-
-	#Tools Common
-	logging				core/lib/logging
-	generic_plugin		core/lib/generic_plugin
-	generic_plugin_manager	core/lib/generic_plugin_manager
-)
-
-SET( BW_BINARY_PROJECTS	
-	# Apps
-	ngt_maya_plugin			core/app/maya_plugin
-)

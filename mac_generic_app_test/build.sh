7de0684bd8af5eca54c24931f7713960016b4221
Charles Yin <c_yin@wargaming.net>
2015-07-22 17:59:29 +1000

    Remove MACOSX CMake command line parameter

--- mac_generic_app_test/build.sh
+++ mac_generic_app_test/build.sh
@@ -1,1 +1,2 @@
-cmake /Users/evgenys/wg/ngt_stash/src -Wno-dev -G"Xcode" -DBW_CMAKE_TARGET=generic_app_test -DQT_VERSION=5.5.0 -DBW_VERIFY_LINUX=OFF -DMACOSX=ON
+BUILD_DIR="$(cd "$(dirname ${0})" && pwd -P )"
+cmake $BUILD_DIR/../src -Wno-dev -G"Xcode" -DBW_CMAKE_TARGET=generic_app_test -DQT_VERSION=5.5.0 -DBW_VERIFY_LINUX=OFF

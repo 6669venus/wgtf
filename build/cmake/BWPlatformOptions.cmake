efc8783d484d5cfdead37394de3eb896fa177501
Charles Yin <c_yin@wargaming.net>
2015-08-03 15:13:27 +1000

    Refactor CMake scripts by removing more BW specific logic
    1. Remove BW server remote build logic in src/CMakeLists.txt
    2. Add vs2015 compiler tag
    3. Move Platform options into cmake/BWPlatformOptions.cmake

---
+++ build/cmake/BWPlatformOptions.cmake
@@ -0,0 +1,17 @@
+# Determine platform
+IF( WIN32 )
+	SET( BW_PLATFORM_WINDOWS ON )
+	IF( CMAKE_SIZEOF_VOID_P EQUAL 4 )
+		SET( BW_PLATFORM "win32" )
+		SET( BW_ARCH_32 ON )
+	ELSEIF( CMAKE_SIZEOF_VOID_P EQUAL 8 )
+		SET( BW_PLATFORM "win64" )
+		SET( BW_ARCH_64 ON )
+	ELSE()
+		MESSAGE( FATAL_ERROR "Only 32 and 64 bit builds are supported." )
+	ENDIF()
+ELSE()
+	MESSAGE( FATAL_ERROR "Only Windows builds are currently supported." )
+ENDIF()
+MESSAGE( STATUS "Platform: ${BW_PLATFORM}" )
+

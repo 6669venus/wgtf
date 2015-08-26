264131d23894ae4c67189e6cd08fe92fe6fc49c5
Evgeny Strokov <e_strokov@wargaming.net>
2015-08-26 16:56:51 +1000

    References instead of allocation in signal.hpp for loops

--- src/core/lib/core_common/signal.hpp
+++ src/core/lib/core_common/signal.hpp
@@ -22,7 +22,7 @@ class Signal
 public:
 	void operator()()
 	{
-		for ( auto slot : m_slots )
+		for ( auto& slot : m_slots )
 		{
 			if ( slot )
 				slot();
@@ -32,7 +32,7 @@ public:
 	template<class TArg1>
 	void operator()(TArg1 arg1)
 	{
-		for ( auto slot : m_slots )
+		for ( auto& slot : m_slots )
 		{
 			if ( slot )
 				slot(arg1);
@@ -42,7 +42,7 @@ public:
 	template<class TArg1, class TArg2>
 	void operator()(TArg1 arg1, TArg2 arg2)
 	{
-		for ( auto slot : m_slots )
+		for ( auto& slot : m_slots )
 		{
 			if ( slot )
 				slot(arg1, arg2);
@@ -52,7 +52,7 @@ public:
 	template<class TArg1, class TArg2, class TArg3>
 	void operator()(TArg1 arg1, TArg2 arg2, TArg3 arg3)
 	{
-		for ( auto slot : m_slots )
+		for ( auto& slot : m_slots )
 		{
 			if ( slot )
 				slot(arg1, arg2, arg3);
@@ -62,7 +62,7 @@ public:
 	template<class TArg1, class TArg2, class TArg3, class TArg4>
 	void operator()(TArg1 arg1, TArg2 arg2, TArg3 arg3, TArg4 arg4)
 	{
-		for ( auto slot : m_slots )
+		for ( auto& slot : m_slots )
 		{
 			if ( slot )
 				slot(arg1, arg2, arg3, arg4);
@@ -72,7 +72,7 @@ public:
 	template<class TArg1, class TArg2, class TArg3, class TArg4, class TArg5>
 	void operator()(TArg1 arg1, TArg2 arg2, TArg3 arg3, TArg5 arg5)
 	{
-		for ( auto slot : m_slots )
+		for ( auto& slot : m_slots )
 		{
 			if ( slot )
 				slot(arg1, arg2, arg3, arg4, arg5);

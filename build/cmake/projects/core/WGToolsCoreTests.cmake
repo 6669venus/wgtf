a32bdce334deb3577f54a07c77bd7e17e8e0bef4
Georgia Nelson <g_nelson@wargaming.net>
2015-08-27 08:51:55 -0500

    Removing the example plugin so that we can merge into develop the fixes Siegi made.

--- build/cmake/projects/core/WGToolsCoreTests.cmake
+++ build/cmake/projects/core/WGToolsCoreTests.cmake
@@ -62,7 +62,6 @@ LIST( APPEND BW_PLUGIN_PROJECTS
 	plg_obj_handle_test			core/testing/plg_obj_handle_test
 	plg_panel_manager_test		core/testing/plg_panel_manager_test
 	plg_test_active_filters		core/testing/plg_test_active_filters
-	plg_example_listfilter_bug	core/testing/plg_example_listfilter_bug
 	plg_test_panel_manager		core/testing/plg_test_panel_manager
 	
 	# Unit test plugins

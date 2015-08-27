8368ed26c175eaffb4592d93adacc96ab940762a
Shaohua Yuan <s_yuan@wargaming.net>
2015-08-27 13:19:44 +1000

    add default implementation for newly added function in IWindow to avoid compile errors in which source is out of ngt core scope checked by: ben

--- src/core/lib/core_ui_framework/i_window.hpp
+++ src/core/lib/core_ui_framework/i_window.hpp
@@ -22,7 +22,7 @@ public:
 	virtual void close() = 0;
 
 	virtual void show() = 0;
-	virtual void showModal() = 0;
+	virtual void showModal() {}
 	virtual void hide() = 0;
 
 	virtual const Menus & menus() const = 0;

560ccc80c0234c6f56999714c8dced4dcd99b73a
g_nelson <g_nelson@wargaming.net>
2015-09-24 17:13:54 +1000

    Removing extra panel manager test plugin.

--- build/cmake/projects/core/WGToolsCoreTests.cmake
+++ build/cmake/projects/core/WGToolsCoreTests.cmake
@@ -63,7 +63,6 @@ LIST( APPEND BW_PLUGIN_PROJECTS
 	plg_obj_handle_test			core/testing/plg_obj_handle_test
 	plg_panel_manager_test		core/testing/plg_panel_manager_test
 	plg_test_active_filters		core/testing/plg_test_active_filters
-	plg_test_panel_manager		core/testing/plg_test_panel_manager
 	
 	# Unit test plugins
 	${BW_TOOLS_UNIT_TEST_PLUGINS}

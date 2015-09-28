14b519be7734a60b61fbc1908bfb0ca1a9c8e950
y_rakhuba <y_rakhuba@wargaming.net>
2015-09-28 14:32:12 +0300

    fixed crash when source model == nullptr probably it must be done another way

--- src/core/lib/core_qt_common/helpers/wg_filtered_tree_model.cpp
+++ src/core/lib/core_qt_common/helpers/wg_filtered_tree_model.cpp
@@ -105,3 +105,19 @@ void WGFilteredTreeModel::setFilter( QObject * filter )
 	impl_->setFilter( wgFilter );
 }
 
+int WGFilteredTreeModel::rowCount(const QModelIndex &parent) const
+{
+    ITreeModel* model = getModel();
+    if (model == nullptr || parent.column() > 0)
+    {
+        return 0;
+    }
+
+    auto filteredModel = static_cast<FilteredTreeModel*>(model);
+    if (filteredModel->getSource() == nullptr)
+    {
+        return 0;
+    }
+
+    return WGTreeModel::rowCount(parent);
+}

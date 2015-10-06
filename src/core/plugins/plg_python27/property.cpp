a231b4e07199eb6b22a3c8d13896753a21b5e0a9
Jacqueline Gurto <j_gurto@wargaming.net>
2015-10-06 11:45:04 +1100

    Change the throw to an assert in methods that are not implemented. throw std::logic_error( "The method or operation is not implemented." ); ---> assert( false && "The method or operation is not implemented." ); Add the bug number to a TODO comment.

--- src/core/plugins/plg_python27/property.cpp
+++ src/core/plugins/plg_python27/property.cpp
@@ -2,7 +2,9 @@
 #include "property.hpp"
 
 Property::Property( const char* name )
-	// TODO name only, needs values and types
+	// TODO NGT-1161
+	// At the moment I'm only storing the name of the variable.
+	// Need to implement storage of values and types.
 	: BaseProperty( name, TypeId( "Python" ) )
 {
 }

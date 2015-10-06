f0cbcdeca799ac3b93331c221eaff324334062d1
Charles Yin <c_yin@wargaming.net>
2015-10-06 12:47:46 +1100

    Change the plugin config path to be nullptr by default

--- src/core/app/generic_app/app/command_line_parser.cpp
+++ src/core/app/generic_app/app/command_line_parser.cpp
@@ -6,19 +6,18 @@
 CommandLineParser::CommandLineParser(int argc, char** argv)
 	: argc_( argc )
 	, argv_( argv )
-	, configPath_( "" )
+	, configPath_( nullptr )
 {
 	for (int i = 0; i < argc_ - 1; ++i)
 	{
 		if (::strncmp( argv_[ i ], "--config", sizeof( "--config" ) ) == 0)
 		{
 			configPath_ = argv_[ i + 1 ];
+			std::wstring_convert< std::codecvt_utf8< wchar_t > > conv;
+			configPathW_ = conv.from_bytes(configPath_);
 			break;
 		}
 	}
-
-	std::wstring_convert< std::codecvt_utf8< wchar_t > > conv;
-	configPathW_ = conv.from_bytes( configPath_ );
 }
 
 //==============================================================================
@@ -43,6 +42,6 @@ const char* CommandLineParser::pluginConfigPath() const
 const wchar_t* CommandLineParser::pluginConfigPathW() const
 {
 
-	return configPathW_.c_str();
+	return configPathW_.empty() ? nullptr : configPathW_.c_str();
 
 }

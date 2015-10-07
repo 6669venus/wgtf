a500ebf68298285e89889e7c8463797cdf3badf5
Shaohua Yuan <s_yuan@wargaming.net>
2015-10-07 17:10:42 +1100

    hook tree model and list model test in new plugins

--- src/core/testing/plg_list_model_test/plg_list_model_test.cpp
+++ src/core/testing/plg_list_model_test/plg_list_model_test.cpp
@@ -2,10 +2,10 @@
 #include "core_generic_plugin/interfaces/i_application.hpp"
 #include "core_generic_plugin/generic_plugin.hpp"
 #include "core_variant/variant.hpp"
-
+#include "core_ui_framework/i_view.hpp"
 #include "core_ui_framework/i_ui_application.hpp"
 #include "core_ui_framework/i_ui_framework.hpp"
-
+#include "test_list_model.hpp"
 #include <vector>
 
 
@@ -15,6 +15,8 @@ class ListModelTestPlugin
 {
 private:
 	std::vector<IInterface*> types_;
+	std::unique_ptr<IView> listView_;
+	std::unique_ptr<IView> shortListView_;
 public:
 	//==========================================================================
 	ListModelTestPlugin(IComponentContext & contextManager )
@@ -31,11 +33,36 @@ public:
 	//==========================================================================
 	void Initialise( IComponentContext & contextManager )
 	{
+		Variant::setMetaTypeManager( 
+			contextManager.queryInterface< IMetaTypeManager >() );
+
+		auto uiApplication = contextManager.queryInterface< IUIApplication >();
+		auto uiFramework = contextManager.queryInterface< IUIFramework >();
+		assert( (uiFramework != nullptr) && (uiApplication != nullptr) );
+
+		std::unique_ptr< IListModel > listModel( new TestListModel() );
+		listView_ = uiFramework->createView(
+			"qrc:///listmodel_testing/test_list_panel.qml",
+			IUIFramework::ResourceType::Url, std::move( listModel ) );
+
+		std::unique_ptr< IListModel > shortListModel( new TestListModel( true ) );
+		shortListView_ = uiFramework->createView(
+			"qrc:///listmodel_testing/test_list_panel.qml",
+			IUIFramework::ResourceType::Url, std::move( shortListModel ) );
+
+		uiApplication->addView( *listView_ );
+		uiApplication->addView( *shortListView_ );
 
 	}
 	//==========================================================================
 	bool Finalise( IComponentContext & contextManager )
 	{
+		auto uiApplication = contextManager.queryInterface< IUIApplication >();
+		assert( uiApplication != nullptr );
+		uiApplication->removeView( *shortListView_ );
+		uiApplication->removeView( *listView_ );
+		listView_ = nullptr;
+		shortListView_ = nullptr;
 		return true;
 	}
 	//==========================================================================
@@ -45,11 +72,6 @@ public:
 		{
 			contextManager.deregisterInterface( type );
 		}
-	}
-
-	void initReflectedTypes( IDefinitionManager & definitionManager )
-	{
-
 	}
 
 };
--- src/core/testing/plg_tree_model_test/plg_tree_model_test.cpp
+++ src/core/testing/plg_tree_model_test/plg_tree_model_test.cpp
@@ -2,12 +2,10 @@
 #include "core_generic_plugin/interfaces/i_application.hpp"
 #include "core_generic_plugin/generic_plugin.hpp"
 #include "core_variant/variant.hpp"
-
 #include "core_ui_framework/i_ui_application.hpp"
 #include "core_ui_framework/i_ui_framework.hpp"
-
-
-
+#include "core_ui_framework/i_view.hpp"
+#include "test_tree_model.hpp"
 #include <vector>
 
 
@@ -17,6 +15,7 @@ class TreeModelTestPlugin
 {
 private:
 	std::vector<IInterface*> types_;
+	std::unique_ptr<IView> treeView_;
 public:
 	//==========================================================================
 	TreeModelTestPlugin(IComponentContext & contextManager )
@@ -33,11 +32,27 @@ public:
 	//==========================================================================
 	void Initialise( IComponentContext & contextManager )
 	{
+		Variant::setMetaTypeManager( 
+			contextManager.queryInterface< IMetaTypeManager >() );
 
+		auto uiApplication = contextManager.queryInterface< IUIApplication >();
+		auto uiFramework = contextManager.queryInterface< IUIFramework >();
+		assert( (uiFramework != nullptr) && (uiApplication != nullptr) );
+
+		auto model = std::unique_ptr< ITreeModel >( new TestTreeModel() );
+		treeView_ = uiFramework->createView( 
+			"qrc:///treemodel_testing/test_tree_panel.qml",
+			IUIFramework::ResourceType::Url, std::move( model ) );
+
+		uiApplication->addView( *treeView_ );
 	}
 	//==========================================================================
 	bool Finalise( IComponentContext & contextManager )
 	{
+		auto uiApplication = contextManager.queryInterface< IUIApplication >();
+		assert( uiApplication != nullptr );
+		uiApplication->removeView( *treeView_ );
+		treeView_ = nullptr;
 		return true;
 	}
 	//==========================================================================
@@ -47,11 +62,6 @@ public:
 		{
 			contextManager.deregisterInterface( type );
 		}
-	}
-
-	void initReflectedTypes( IDefinitionManager & definitionManager )
-	{
-
 	}
 
 };

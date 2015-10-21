03076b29ff26a1466ad6394c947c35f74a9b69f2
Georgia Nelson <g_nelson@wargaming.net>
2015-10-21 11:15:12 -0500

    Creating and adding the context menu test action to the application

--- src/core/testing/plg_context_menu_test/plg_context_menu_test.cpp
+++ src/core/testing/plg_context_menu_test/plg_context_menu_test.cpp
@@ -1,6 +1,7 @@
 #include "core_dependency_system/i_interface.hpp"
 #include "core_generic_plugin/generic_plugin.hpp"
 #include "core_qt_common/i_qt_framework.hpp"
+#include "core_ui_framework/i_action.hpp"
 #include "core_ui_framework/i_ui_application.hpp"
 #include "core_ui_framework/i_ui_framework.hpp"
 #include "core_ui_framework/i_window.hpp"
@@ -24,9 +25,28 @@ public:
 	void Initialise( IComponentContext & contextManager )
 	{
 		auto uiFramework = contextManager.queryInterface< IUIFramework >();
+		assert( uiFramework );
 
+		auto uiApplication = contextManager.queryInterface< IUIApplication >();
+		assert( uiApplication );
+
+		// Load the action data
 		uiFramework->loadActionData( ":/testing/actiondata", IUIFramework::ResourceType::File );
+
+		// Create an action and add it to the UI Application
+		contextMenuTest1_ = uiFramework->createAction( "ContextMenuTest1", 
+														std::bind( &ContextMenuTest::executeTest1, this ) );
+		uiApplication->addAction( *contextMenuTest1_ );
 	}
+		
+	//==========================================================================
+	void executeTest1()
+	{
+	}
+
+private:
+	
+	std::unique_ptr< IAction > contextMenuTest1_;
 };
 
 PLG_CALLBACK_FUNC( ContextMenuTest )

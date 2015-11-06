740a1ceda85e4e5f5c062bf4fffb0fc6150dc3dc
Jacqueline Gurto <j_gurto@wargaming.net>
2015-11-06 14:24:07 +1100

    Remove a typo "+ +".

--- src/core/lib/core_reflection/class_definition.cpp
+++ src/core/lib/core_reflection/class_definition.cpp
@@ -411,7 +411,7 @@ void ClassDefinition::bindPropertyImpl(
 			ceh, collection,
 			nameBuffer,
 			newBegin, indexEnd))
-				{
+		{
 			std::shared_ptr< CollectionElementHolder > overrideBaseProperty(
 				new CollectionElementHolder( std::move( ceh ) ) );
 			overrideBaseProperty->setName( nameBuffer );
@@ -430,8 +430,11 @@ void ClassDefinition::bindPropertyImpl(
 		{
 			return;
 		}
+		// Increment indexEnd past "]."
+		const size_t indexOffset = 2;
+		const char * childName = indexEnd + indexOffset;
 		return definition->bindPropertyImpl(
-			indexEnd + + strlen( "]" ) + strlen( "." ),
+			childName,
 			baseProvider,
 			o_PropertyAccessor );
 	}

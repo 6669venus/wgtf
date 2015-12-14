dbc2004ebbd094c11f8d3da089755e047d62c3b8
Jacqueline Gurto <j_gurto@wargaming.net>
2015-12-14 11:18:40 +1100

    Fix for duplicate definitions being added for the same ScriptObject instance. Do a deep compare on ScriptObjects in ScriptObjectDefinitionRegistry. NGT-1613

--- src/core/lib/core_python27/script_object_definition_registry.hpp
+++ src/core/lib/core_python27/script_object_definition_registry.hpp
@@ -12,6 +12,11 @@ class IComponentContext;
 struct ScriptObjectDefinitionDeleter;
 
 
+/**
+ *	Stores an IClassDefinition for each PyScript::ScriptObject instance.
+ *	So that duplicate IClassDefinitions are created for the same
+ *	PyScript::ScriptObject instance.
+ */
 class ScriptObjectDefinitionRegistry: public Implements<IScriptObjectDefinitionRegistry>
 {
 public:
@@ -20,6 +25,14 @@ public:
 	{}
 
 
+	/**
+	 *	Find an existing or add a new definition for the given object.
+	 *	Definitions will automatically be removed when the reference count
+	 *	on the std::shared_ptr reaches 0.
+	 *	@param object the PyScript::ScriptObject for which to find the definition.
+	 *		Must not be null.
+	 *	@return an existing definition or a newly added definition.
+	 */
 	virtual std::shared_ptr<IClassDefinition> getDefinition( const PyScript::ScriptObject& object ) override;
 
 
@@ -28,7 +41,24 @@ private:
 
 	friend struct ScriptObjectDefinitionDeleter;
 
-	std::map<PyScript::ScriptObject, std::weak_ptr<IClassDefinition>> definitions_;
+	/**
+	 *	Key compare functor.
+	 *	Need to do a deep compare on PyScript::ScriptObject to prevent getting
+	 *	copies of the same object added to the map.
+	 */
+	class ScriptObjectCompare
+	{
+	public:
+		bool operator()( const PyScript::ScriptObject & a,
+			const PyScript::ScriptObject & b ) const
+		{
+			return a.compareTo( b, PyScript::ScriptErrorPrint() ) < 0;
+		}
+	};
+	typedef std::map< PyScript::ScriptObject,
+		std::weak_ptr< IClassDefinition >,
+		ScriptObjectCompare > DefinitionMap;
+	DefinitionMap definitions_;
 	std::mutex definitionsMutex_;
 	IComponentContext& context_;
 };

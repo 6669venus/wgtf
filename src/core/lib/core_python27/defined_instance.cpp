36ab93884488f81492549dda59622bafdca1a276
Jacqueline Gurto <j_gurto@wargaming.net>
2016-02-03 12:28:54 +1100

    Import setattr changes from feature/add-python-properties-NGT-1319-b2. NGT-1516

--- src/core/lib/core_python27/defined_instance.cpp
+++ src/core/lib/core_python27/defined_instance.cpp
@@ -10,6 +10,41 @@
 #include "property.hpp"
 
 
+const char * SETATTR_NAME = "__setattr__";
+const char * OLD_SETATTR_NAME = "__old_setattr__";
+
+static int py_setattr_hook( PyObject * self, PyObject * name, PyObject * value )
+{
+	if (name == nullptr)
+	{
+		PyErr_Format( PyExc_AttributeError,
+			"Cannot set attribute with no name." );
+		return -1;
+	}
+	if (value == nullptr)
+	{
+		PyErr_Format( PyExc_AttributeError,
+			"Cannot set attribute with no value." );
+		return -1;
+	}
+
+	// Call through to original setattr
+	auto selfObject = PyScript::ScriptObject( self,
+		PyScript::ScriptObject::FROM_BORROWED_REFERENCE );
+
+	auto typeObject = PyScript::ScriptType::getType( selfObject );
+
+	auto args = PyScript::ScriptArgs::create( self, name, value );
+	const bool allowNullMethod = false;
+	auto result = typeObject.callMethod( OLD_SETATTR_NAME,
+		args,
+		PyScript::ScriptErrorPrint(),
+		allowNullMethod );
+
+	return 0;
+}
+
+
 namespace ReflectedPython
 {
 
@@ -80,6 +115,76 @@ DefinedInstance::~DefinedInstance()
 	// IObjectManager should take a weak reference
 	handle = objectManager.registerObject( handle, id );
 	assert( handle.isValid() );
+
+	PyScript::ScriptErrorPrint errorPrint;
+	//PyScript::ScriptModule module = PyScript::ScriptModule::import( "inspect",
+	//	PyScript::ScriptErrorPrint( "Unable to import\n" ) );
+	//auto isClassArgs = PyScript::ScriptArgs::create( pythonObject.get() );
+	//const bool allowNullMethod = false;
+	//auto isClass = module.callMethod( "isclass", isClassArgs, errorPrint, allowNullMethod );
+
+	// Hook attribute listeners into object
+	//if (PyScript::ScriptInstance::check( pythonObject ))
+	//{
+	//	// TODO old-style class
+	//}
+	//else if (PyScript::ScriptType::check( pythonObject ))
+	//else if (isClass.exists() && isClass.isTrue( errorPrint ))
+	//if (pythonObject.hasAttribute( SETATTR_NAME ))
+
+	// TODO Just my test class
+	if (strcmp( pythonObject.typeNameOfObject(), "NewClass" ) == 0)
+	{
+		auto pyType = pythonObject.get()->ob_type;
+		const auto isAlreadyTracked = (pyType->tp_setattro == py_setattr_hook);
+
+		if (!isAlreadyTracked)
+		{
+			auto typeObject = PyScript::ScriptType::getType( pythonObject );
+
+			// Construct new hook
+			static PyMethodDef s_methods[] =
+			{
+				{
+					SETATTR_NAME,
+					reinterpret_cast< PyCFunction >( &py_setattr_hook ),
+					METH_VARARGS|METH_KEYWORDS,
+					"Listener to notify the NGT Reflection System\n"
+					"x.__setattr__('name', value) <==> x.name = value"
+				},
+				{ nullptr, nullptr, 0, nullptr }
+			};
+
+			auto pyFunction = PyCFunction_New( s_methods, pythonObject.get() );
+			auto functionObject = PyScript::ScriptObject( pyFunction,
+				PyScript::ScriptObject::FROM_NEW_REFERENCE );
+
+			PyObject * self = nullptr;
+			auto pyMethod = PyMethod_New( pyFunction, self, typeObject.get() );
+			auto methodObject = PyScript::ScriptObject( pyMethod,
+				PyScript::ScriptObject::FROM_NEW_REFERENCE );
+
+			// Save old setattr
+			auto oldSetattr = pythonObject.getAttribute( SETATTR_NAME,
+				PyScript::ScriptErrorClear() );
+			const auto hasOldAttr = oldSetattr.exists();
+
+			// Hook uses old version
+			assert( hasOldAttr );
+
+			if (hasOldAttr)
+			{
+				const auto saved = typeObject.setAttribute( OLD_SETATTR_NAME,
+					oldSetattr,
+					errorPrint );
+				assert( saved );
+
+				// TODO work out a way to use a wrapper instead?
+				pyType->tp_setattro = py_setattr_hook;
+				PyType_Modified( pyType );
+			}
+		}
+	}
 
 	// Registered reference
 	return handle;

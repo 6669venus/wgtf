7938c030c221ecc8ddc5a24e5c738fa8b84849ff
Jacqueline Gurto <j_gurto@wargaming.net>
2016-03-07 19:09:06 +1100

    Forgot change in last commit.

--- src/core/testing/plg_python27_interface_test/python27_interface_test.cpp
+++ src/core/testing/plg_python27_interface_test/python27_interface_test.cpp
@@ -104,11 +104,39 @@ TEST( Python27Interface )
 		auto moduleDefinition = module.getDefinition( *definitionManager );
 		auto testDataAccessor = moduleDefinition->bindProperty( "testData", module );
 
-		// Invoke function
-		const auto setResult = testDataAccessor.setValue( 2 );
-		const auto success = setResult && !scriptingEngine->checkErrors();
+		// Set value, notify listeners
+		const auto getResult = testDataAccessor.getValue();
+		int expectedOldValue = 0;
+		const auto getSuccess = getResult.tryCast< int >( expectedOldValue );
+		CHECK( getSuccess );
 
-		CHECK( success );
+		const int expectedNewValue = 2;
+		const auto setResult = testDataAccessor.setValue( expectedNewValue );
+		const auto setSuccess = setResult && !scriptingEngine->checkErrors();
+		CHECK( setSuccess );
+
+		// Check listeners were notified
+		{
+			CHECK( testListener->preSetValueCalled_ );
+			CHECK_EQUAL( testDataAccessor.getType(), testListener->preSetAccessor_.getType() );
+			CHECK( strcmp( testDataAccessor.getName(), testListener->preSetAccessor_.getName() ) == 0 );
+			CHECK_EQUAL( testDataAccessor.getObject(), testListener->preSetAccessor_.getObject() );
+			int preSetValue = 0;
+			const auto preSetValueSuccess = testListener->preSetValue_.tryCast< int >( preSetValue );
+			CHECK( preSetValueSuccess );
+			CHECK_EQUAL( expectedNewValue, preSetValue );
+		}
+
+		{
+			CHECK( testListener->postSetValueCalled_ );
+			CHECK_EQUAL( testDataAccessor.getType(), testListener->postSetAccessor_.getType() );
+			CHECK( strcmp( testDataAccessor.getName(), testListener->postSetAccessor_.getName() ) == 0 );
+			CHECK_EQUAL( testDataAccessor.getObject(), testListener->postSetAccessor_.getObject() );
+			int postSetValue = 0;
+			const auto postSetValueSuccess = testListener->postSetValue_.tryCast< int >( postSetValue );
+			CHECK( postSetValueSuccess );
+			CHECK_EQUAL( expectedNewValue, postSetValue );
+		}
 
 		definitionManager->deregisterPropertyAccessorListener(
 			std::static_pointer_cast< PropertyAccessorListener >( testListener ) );

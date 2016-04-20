dbbd0f60d6334f120c151241bdf4168ade966eb7
Aidan Boneham <a_boneham@wargaming.net>
2016-04-20 16:37:43 +1000

    Split CollectionModelOld to its own hpp/cpp

--- src/core/lib/core_data_model/collection_model.hpp
+++ src/core/lib/core_data_model/collection_model.hpp
@@ -32,30 +32,4 @@ protected:
 };
 
 
-/**
- *	Data model for displaying a Collection as a list.
- */
-class CollectionModelOld
-	: public IListModel
-{
-public:
-	CollectionModelOld();
-	virtual ~CollectionModelOld();
-
-	void setSource( Collection & collection );
-	Collection & getSource();
-
-	// IListModel
-	virtual IItem * item( size_t index ) const override;
-	virtual size_t index( const IItem* item ) const override;
-
-	virtual bool empty() const override;
-	virtual size_t size() const override;
-	virtual int columnCount() const override;
-
-protected:
-	Collection collection_;
-	mutable std::vector< std::unique_ptr< IItem > > items_;
-};
-
 #endif // COLLECTION_LIST_MODEL_HPP

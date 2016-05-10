415f95ddd99ed549c2f99bcd6180e3a03cff3732
e zvi <e_zvi@wargaming.net>
2016-05-10 18:16:29 +1000

    fix-Shell scripts need forward slash

--- build/ci/ci_xcode_qt5.5.1.sh
+++ build/ci/ci_xcode_qt5.5.1.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
-sh ..\bigworld_cmake.sh --target "generic_app_test_deploy" --generator "xcode" --qt-version "5.5.1" 
+sh ../bigworld_cmake.sh --target "generic_app_test_deploy" --generator "xcode" --qt-version "5.5.1" 
 
 cd ../../build_generic_app_test_deploy_xcode_qt5.5.1
 ./rebuild_hybrid.sh

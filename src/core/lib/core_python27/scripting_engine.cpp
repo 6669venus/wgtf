8538c6d5959cfad058875a35446f887f345193b5
Jacqueline Gurto <j_gurto@wargaming.net>
2016-05-31 16:57:02 +1000

    Build fixes from last commit. NGT-2479

--- src/core/lib/core_python27/scripting_engine.cpp
+++ src/core/lib/core_python27/scripting_engine.cpp
@@ -138,12 +138,15 @@ bool Python27ScriptingEngine::init()
 	PyImport_ImportModule( "scriptoutputwriter" );
 
 	// Allow import from supported system modules
-	char * pythonSourcePath[ MAX_PATH ];
-	wsprintf( pythonSourcePath,
+	const size_t BUFFER_SIZE = 256;
+	wchar_t pythonSourcePath[ BUFFER_SIZE ];
+	const auto result = swprintf( pythonSourcePath,
+		BUFFER_SIZE,
 		L"../../../src/core/third_party/python/Python-%d.%d.%d/Lib",
 		PY_MAJOR_VERSION,
 		PY_MINOR_VERSION,
 		PY_MICRO_VERSION );
+	assert( result > 0 );
 	if (!this->appendSourcePath( pythonSourcePath ))
 	{
 		NGT_ERROR_MSG( "Failed to append path to system modules\n" );
